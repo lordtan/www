9270083b2d6b0c2f6bf50988eaa910cd91552e8c
tanluole <tanluole@gmail.com>
2018-03-15 09:31:39 +0800

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,35 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+    	    alert("111");
+    	    //window.location.href="success.html"; //这个是ok的
+    	    cordova.InAppBrowser.open('success.html', '_blank', 'location=no,toolbar=yes,toolbarposition=top,closebuttoncaption=关闭');
+        //document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        alert("222");
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        //this.receivedEvent('deviceready');
+         
+        //测试页面跳转
+        //cordova.InAppBrowser.open('success.html', '_blank', 'location=no,toolbar=yes,toolbarposition=top,closebuttoncaption=关闭');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();  //初始化
